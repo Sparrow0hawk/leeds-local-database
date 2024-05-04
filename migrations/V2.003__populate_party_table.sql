b685b0406193ad3ea62852a683942e4dd195c3d2
Alex Coleman <alexjcoleman@gmail.com>
2024-05-04 15:09:37 +0100

    Add scripts for party table

---
+++ migrations/V2.003__populate_party_table.sql
@@ -0,0 +1,14 @@
+INSERT INTO election.party(name)
+VALUES
+    ('Alliance for Green Socialism'),
+    ('Conservative'),
+    ('Green'),
+    ('Independent'),
+    ('Labour'),
+    ('Labour and Co-operative'),
+    ('Liberal Democrat'),
+    ('Morley Borough Independents'),
+    ('Reform UK'),
+    ('Social Democratic'),
+    ('Trade Union and Socialist Coalition'),
+    ('Yorkshire');
